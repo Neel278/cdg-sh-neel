f106b080babdeb5c438826a6db3910f21ad2f02a
Neel Thakkar <neelthakkar2001@gmail.com>
2021-03-09 11:33:18 +0530

    Initital commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+// app.get("/url/:id", (req, res) => {
+// 	// Todo : get a short url by id
+// });
+
+// app.get("/:id", (req, res) => {
+// 	// Todo : redirect to url
+// });
+
+// app.post("/url", (req, res) => {
+// 	// Todo : create a short url
+// });
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+	console.log(`Listenig at http://localhost:${port}`);
+});
